e4c30aee6b3424def5b007326ac5ae34b70751e5
Douglas Ardila Garcés <dag1702@hotmail.com>
2019-05-21 18:09:00 -0500

    Update database.js

--- src/database.js
+++ src/database.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 mongoose.set('useFindAndModify', false);
-mongoose.connect("mongodb+srv://dag171802:dag171802@clustermongo-dgs5s.mongodb.net/test?retryWrites=true", {
-//mongoose.connect('mongodb://mongo-server/node-notes-db', {
+//mongoose.connect("mongodb+srv://dag171802:dag171802@clustermongo-dgs5s.mongodb.net/test?retryWrites=true", {
+mongoose.connect('mongodb://mongo-server/node-notes-db', {
 //mongoose.connect('mongodb://localhost/node-notes-db', {
   useCreateIndex: true,
   useNewUrlParser: true
